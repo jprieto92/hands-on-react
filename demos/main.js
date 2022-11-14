ed5bca70dee51fb82a333377d2d882bf538e86d5
Javier Prieto <javier.prieto@sysdig.com>
2022-11-14 17:46:58 +0100

    feat: rendering message as component

--- demos/main.js
+++ demos/main.js
@@ -1,19 +1,6 @@
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-
-function renderElement() {
-  const element = (
-    <div className="post">
-      <h1>My First Blog Post</h1>
-      <div>Author: Javier Prieto</div>
-      <div>Published: {new Date().toLocaleTimeString()}</div>
-      <p>
-        I am new to blogging and this is my first post. You should expect a
-        lot of great things from me.
-      </p>
-    </div>
-  );
-
-  root.render(element);
-}
-setInterval(renderElement, 1000);
+function HelloWorld() {
+    return <div className="container">Hello Function Component</div>;
+  }
+  const rootElement = document.getElementById("root");
+  const element = <HelloWorld />;
+  ReactDOM.createRoot(rootElement).render(element);
