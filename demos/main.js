990b906d8b63ee49c25d870361fb53f0cd1cea17
Javier Prieto <javier.prieto@sysdig.com>
2022-11-14 16:41:20 +0100

    refactor: embedding hello expression in JSX

--- demos/main.js
+++ demos/main.js
@@ -1,4 +1,7 @@
-const rootElement = document.getElementById("root");
-
-const element = <div className="container">Hello World</div>;
+const rootElement = document.getElementById('root');
+const person = { first: 'Joe', last: 'Doe' };
+const element = 
+    <div className="container">
+        Hello {person.first} {person.last}
+    </div>;
 ReactDOM.createRoot(rootElement).render(element);
