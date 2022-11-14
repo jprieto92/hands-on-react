dba2ce636711efaacfeb7d13fff406ee4e3caa8b
Javier Prieto <javier.prieto@sysdig.com>
2022-11-14 13:11:16 +0100

    refactor: update code to use React

--- demos/main.js
+++ demos/main.js
@@ -1,6 +1,12 @@
 const rootElement = document.getElementById("root");
 
-const element = document.createElement("div");
-element.textContent = "Hello World";
-element.className = "container";
-rootElement.appendChild(element);
+const element = React.createElement(
+    "div",
+    {
+        className: "container",
+    },
+    "Hello World"
+);
+
+console.log(element);
+ReactDOM.createRoot(rootElement).render(element);
